bb20d4c272153f366398d518b06476a5c1998f09
hichamdandan23 <hdandan@miu.edu>
2020-12-10 16:15:01 -0600

    first

---
+++ src/main/java/com/demo/controllers/KafkaController.java
@@ -0,0 +1,30 @@
+package com.demo.controllers;
+
+import com.demo.engine.Producer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+
+   /* private final Producer producer;
+
+    @Autowired
+    KafkaController(Producer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping(value = "/publish")
+    public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
+
+        this.producer.sendMessage(message);
+        return;
+    }*/
+
+    @GetMapping(value = "/")
+    public String test() {
+
+        return "Hi Mohamed";
+    }
+}
